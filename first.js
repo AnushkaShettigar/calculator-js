99ebf6f578720a5bb3cc799c9c0e412299b2621f
AnushkaShettigar <anushka.shettigar.1706@gmail.com>
2025-11-13 23:57:29 +0530

    stilldk

--- first.js
+++
@@ -1,9 +0,0 @@
-class user{
-    constructor(name, email){
-        this.name = name;
-        this.email = email;
-    }
-     viewDetails(){
-        console.log(`Name: ${this.name}, Email: ${this.email}`);
-     }
-}
